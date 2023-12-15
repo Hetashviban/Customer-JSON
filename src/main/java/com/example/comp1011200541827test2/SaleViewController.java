7b69bf165da3f04e4f95d1d2f77d81af2fc7704a
Hetashvi Ban <hetashvi7988@gmail.com>
2023-12-15 13:27:10 -0500

    Question 2a complete

---
+++ src/main/java/com/example/comp1011200541827test2/SaleViewController.java
@@ -0,0 +1,51 @@
+package com.example.comp1011200541827test2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.image.ImageView;
+
+public class SaleViewController {
+
+    @FXML
+    private TableColumn<?, ?> cityColumn;
+
+    @FXML
+    private TableColumn<?, ?> firstNameColumn;
+
+    @FXML
+    private TableColumn<?, ?> idColumn;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private TableColumn<?, ?> lastNameColumn;
+
+    @FXML
+    private TableColumn<?, ?> purchasesColumn;
+
+    @FXML
+    private Label retailPriceLabel;
+
+    @FXML
+    private Label salePriceLabel;
+
+    @FXML
+    private Label savingsLabel;
+
+    @FXML
+    void allCustomersButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void savedMoneyButton(ActionEvent event) {
+
+    }
+
+}

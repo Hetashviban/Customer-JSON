ba5d9ec1286ecb058e409e686cceadd396679335
Hetashvi Ban <hetashvi7988@gmail.com>
2023-12-15 12:03:32 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.comp1011200541827test2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.comp1011200541827test2 to javafx.fxml;
+    exports com.example.comp1011200541827test2;
+}
